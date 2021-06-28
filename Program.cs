ab381750d5a3c4d69fc9d446eeaeb9f129e7dc9d
[Sona] <[sona16061999@gmail.com]>
2021-06-28 13:34:32 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OOpsPractice
+{
+
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            int empPresent = 1;
+            int empAbsent = 0;
+            Random random = new Random();
+            int empInput = random.Next(0, 2);
+            if (empInput == 1)
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+
+        }
+    }
+
+}
+    
+    
+           

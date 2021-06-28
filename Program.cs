e6a6750d70407c888d8db3b5ecb22e111325d3ac
[Sona] <[sona16061999@gmail.com]>
2021-06-28 19:51:17 +0530

    Employee Part Time

--- Program.cs
+++ Program.cs
@@ -6,24 +6,39 @@ namespace OOpsPractice
 
     class Program
     {
-
         public static void Main(string[] args)
         {
-            int empPresent = 1;
-            
+            int partTime = 2;
+            int fullTime = 1;
+            int empHour = 0;
+            int empWage = 0;
+            int empPerHour = 20;
             Random random = new Random();
-            int empInput = random.Next(0, 2);
-            if (empInput == 1)
+            int empInput = random.Next(0, 3);
+            if (empInput == fullTime)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is Working Fulltime");
 
-                
-          
+                empHour = 9;
+
+            }
+            else if (empInput == partTime)
+            {
+                Console.WriteLine("Employee is Working Partime");
+
+                empHour = 4;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+
+                empHour = 0;
             }
+
+            empWage = empPerHour * empHour;
+            Console.WriteLine("The daily wage of the employee = " + empWage);
+
+
 
 
         }
